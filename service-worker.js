5c94733a379834b72e0f15ddc7cd4bf02005c862
VCore <197873578+vCore420@users.noreply.github.com>
2026-08-02 06:38:34 +1200

    Update cache version from v3 to v4

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open('cynrith-cache-v3').then(cache => 
+    caches.open('cynrith-cache-v4').then(cache => 
       cache.match(event.request).then(response => 
         response || fetch(event.request).then(networkResponse => {
           // Only cache GET requests
